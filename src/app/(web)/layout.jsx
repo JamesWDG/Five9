c43f3fd07b1ebc72c40c5de216c36fd0e84fe5d0
James Howard <james@webdesignglory.com>
2026-02-19 01:38:43 +0500

    Update layout.jsx
    asd

--- src/app/(web)/layout.jsx
+++ src/app/(web)/layout.jsx
@@ -18,7 +18,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <!-- Google tag (gtag.js) --> 
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-9W3BMQ0C6N"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date());
        gtag('config', 'G-9W3BMQ0C6N'); </script>
  
