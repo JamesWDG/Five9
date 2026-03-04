6321a2d6dd8bbe4fc5d34801a8b1ddec25c9603e
James Howard <james@webdesignglory.com>
2026-03-05 03:35:04 +0500

    Update layout.jsx

--- src/app/(web)/layout.jsx
+++ src/app/(web)/layout.jsx
@@ -16,6 +16,9 @@ export const metadata = {
   icons: {
     icon: "/images/footer-logo.png",
   },
+  verification: {
+    google: "qi-YYpeUo7yZFyFYidtfhWcufy_gRrbWjJik-d1d8h4",
+  },
   other: {
     "dmca-site-verification": "bW9wb1VkVXRqWVNyeDdNR2RLbmxUUT090",
   },
@@ -33,19 +36,40 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
-        {/* Google Analytics Script */}
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5K2QQ2Z6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-9W3BMQ0C6N"
+          src="https://www.googletagmanager.com/gtag/js?id=G-VT6PTHXSTV"
           strategy="afterInteractive"
         />
 
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="ga-script" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-9W3BMQ0C6N');
+            gtag('config', 'G-VT6PTHXSTV');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5K2QQ2Z6');
           `}
         </Script>
 
@@ -53,6 +77,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <AIChatbot />
+
       </body>
     </html>
   );
